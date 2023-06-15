287d289438122d0ab0956ba6551ce24a4e1986b4
Tom Ames <tsames1@gmail.com>
2023-06-14 18:30:28 -0700

    Adding routing between distinct screens with React-Navigation. Added the NavigationContainer to App.tsx.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+
+
+    </NavigationContainer>
 }
 
-// test test first pull request
 const styles = StyleSheet.create({
   container: {
     flex: 1,
