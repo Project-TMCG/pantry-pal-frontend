6444a43a3cc1c761532a2c673c4d9357a4ee1112
Tom Ames <tsames1@gmail.com>
2023-06-15 19:52:25 -0700

    Minor adjustments to routing.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
 
+//Example Landing Screen
+function Landing() {
+  return (
+    <View style={styles.container}>
+      <Text>Landing Page</Text>
+    </View>
+  )
+}
+
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
     <NavigationContainer>
-
+      <Stack.Navigator>
+        <Stack.Screen name="Landing" component={Landing} />
+        <Stack.Screen name="Search" component={Landing} />
+        <Stack.Screen name="Results" component={Landing} />
+        <Stack.Screen name="Details" component={Landing} />
+      </Stack.Navigator>
 
     </NavigationContainer>
 }
