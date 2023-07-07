605a13a793aa046aec4e734e01aad435eaa3f843
Koa Garces <koajgarces@yahoo.com>
2023-07-06 19:27:23 -0700

    Set up functionality to pass data to redux for a mock addIngredient function. This will be the skeleton for the actual feature

---
+++ src/components/redux-test-components/addIngredient.tsx
@@ -0,0 +1,58 @@
+//Import Dependencies
+import React, { useState } from "react";
+import { StyleSheet, Text, Button, View, TextInput } from "react-native";
+
+//Import Redux Store & Action Dispatch
+import { useSelector, useDispatch } from "react-redux";
+import { addProduce } from "../../redux/features/selector/selectorSlice";
+
+//Declare and Import Types
+interface Props {}
+
+const AddIngredient: React.FC<Props> = ({}) => {
+  //Redux State Management
+  const selected = useSelector((state: any) => state.selector.produce);
+  const dispatch = useDispatch();
+  //Mock Data to choose from
+  const listOfIngredients: string[] = ["bok Choy ", " eggplant ", " brocolli"];
+  //object that temporarily stores selected ingredients
+  let ingredientsAdded: { [key: string]: number } = {};
+
+  const addIngredient = (ingredient: string): void => {
+    //deletes ingredient if found in object
+    if (ingredientsAdded.hasOwnProperty(ingredient)) {
+      delete ingredientsAdded[ingredient];
+    } else {
+      //adds ingredient to the object in the form of a key if not selected yet
+      ingredientsAdded[ingredient] = 1;
+    }
+  };
+
+  const submitIngredients = (): void => {
+    //converts object into an array of keys and sends it into redux(global state)
+    const arrToObjects = Object.keys(ingredientsAdded);
+    dispatch(addProduce(arrToObjects));
+  };
+
+  return (
+    <View style={styles.container}>
+      {listOfIngredients.map((one) => {
+        return (
+          <Button title={one} key={one} onPress={() => addIngredient(one)} />
+        );
+      })}
+      <Button title="send to redux" onPress={() => submitIngredients()} />
+      <Text>{selected}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default AddIngredient;
--- src/containers/Landing.tsx
+++ src/containers/Landing.tsx
@@ -1,33 +1,39 @@
 //Import Dependencies
-import * as React from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { StyleSheet, Button, Text, View } from 'react-native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import * as React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Button, Text, View } from "react-native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 //Types for React Navigation
-import { RootStackParams } from './../../App';
+import { RootStackParams } from "./../../App";
 
 //Import Components
-import Counter from './../components/redux-test-components/counter'
+import Counter from "./../components/redux-test-components/counter";
+import AddIngredient from "../components/redux-test-components/addIngredient";
 
 const Landing: React.FC = () => {
-  const navigation = useNavigation<NativeStackNavigationProp<RootStackParams>>();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParams>>();
 
   return (
     <View style={styles.container}>
       <Text>Landing Page</Text>
-      <Button title="Get Started" onPress={() => navigation.navigate("Search")}/>
+      <Button
+        title="Get Started"
+        onPress={() => navigation.navigate("Search")}
+      />
       <Counter></Counter>
+      <AddIngredient />
     </View>
-  )
-}
-  
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
