2ddc0d5f393c9aea4e8fea1cafc4e7e21170f5db
mmarvin3 <mmerida336@gmail.com>
2023-09-26 17:31:01 -0400

    Styling fix for the Recipe Details screen title header

--- src/components/form-components/ActiveRecipeName.tsx
+++ src/components/form-components/ActiveRecipeName.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import { Text } from "react-native-elements";
 import { useSelector } from "react-redux";
+import { Dimensions } from "react-native";
+
+const screenWidth = Dimensions.get("screen").width
 
 const ActiceRecipeName = () => {
   const activeName = useSelector((state: any) => state.recipe.activeRecipe);
@@ -15,7 +18,12 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 20,
     fontWeight: "400",
+    flexWrap: 'wrap',
+    width: screenWidth * 0.75
   },
+  headerBlock:{
+    
+  }
 });
 
 export default ActiceRecipeName;
